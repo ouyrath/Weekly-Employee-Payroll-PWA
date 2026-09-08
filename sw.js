af1e909059d09206794ef2e3807bc98434948c99
ouyrath <ouyrath@gmail.com>
2026-09-08 11:57:24 -0500

    Refresh PWA cache for payroll pay types

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='weekly-payroll-v1';
+const CACHE='weekly-payroll-v2';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
